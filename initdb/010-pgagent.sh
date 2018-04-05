6c3ec30e438d9988d2d00f8c792cc5b952bc4906
huggla <henrik.uggla@kristianstad.se>
2018-04-05 14:21:13 +0200

    Update 010-pgagent.sh

--- initdb/010-pgagent.sh
+++ initdb/010-pgagent.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 set -e
 IFS=","
-if [ -z "$initdb_file" ]
-then
-   initdb_file="$(/usr/bin/lsof +p $$ | /bin/grep "initdb" | /usr/bin/awk -F "\t" '{print $3}')"
-fi
-sql_file="$initdb_file.sql"
+sql_file="$sql_dir/$(/usr/bin/basename "$initdb_file").sql"
 >"$sql_file"
 readonly CREATE_EXTENSION_PGAGENT
 if [ "$CREATE_EXTENSION_PGAGENT" == "yes" ]
