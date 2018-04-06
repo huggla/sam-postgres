b808a3eb44a0b36a7448132a0c8d2bbce2da9917
huggla <henrik.uggla@kristianstad.se>
2018-04-06 09:50:24 +0200

    Update and rename 010.postgres.sh to 010.pgagent.sh

---
+++ initdb/010.pgagent.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# set -e +a +m +s +i -f
+# readonly BIN_DIR="$(/usr/bin/dirname "$0")"
+# . "$BIN_DIR/start.stage2.functions"
+
+readonly CREATE_EXTENSION_PGAGENT="$(var - CREATE_EXTENSION_PGAGENT)"
+if [ "$CREATE_EXTENSION_PGAGENT" == "yes" ]
+then
+   readonly CREATE_LANGUAGE_PLPGSQL="yes"
+   prio="010"
+   dbname="postgres"
+   sql_file="$prio.$dbname.sql"
+   echo "CREATE EXTENSION IF NOT EXISTS pgagent;" > "$sql_file"
+fi
