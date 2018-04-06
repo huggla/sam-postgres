3e5ed78c4512a8d010e9bbf7e66ed06bec834e84
huggla <henrik.uggla@kristianstad.se>
2018-04-06 13:01:04 +0200

    Update 010.pgagent.sh

--- initdb/010.pgagent.sh
+++ initdb/010.pgagent.sh
@@ -5,6 +5,7 @@
 # set -e +a +m +s +i +f
 # readonly BIN_DIR="$(/usr/bin/dirname "$0")"
 # . "$BIN_DIR/start.stage2.functions"
+# readonly NAME="$(var - NAME)"
 # readonly CONFIG_FILE="$(var - CONFIG_FILE)"
 # readonly CONFIG_DIR="$(/usr/bin/dirname "$CONFIG_FILE")"
 # readonly sql_dir="$CONFIG_DIR/initdb/sql"
@@ -18,7 +19,7 @@ if [ "$CREATE_EXTENSION_PGAGENT" == "yes" ]
 then
    readonly CREATE_LANGUAGE_PLPGSQL="yes"
    prio="010"
-   dbname="postgres"
+   dbname="$NAME"
    sql_file="$sql_dir/$prio.$dbname.sql"
    echo "CREATE EXTENSION IF NOT EXISTS pgagent;" > "$sql_file"
 fi
