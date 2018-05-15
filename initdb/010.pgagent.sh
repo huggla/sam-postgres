40148b3f6b41fef3fd5744737bd12de48ea3fd74
huggla <henrik.uggla@kristianstad.se>
2018-05-15 15:04:25 +0000

    Update 010.pgagent.sh

--- initdb/010.pgagent.sh
+++ initdb/010.pgagent.sh
@@ -3,16 +3,17 @@
 # Set in parent script:
 # ---------------------------------------------------------
 # set -e +a +m +s +i +f
-# readonly BIN_DIR="$(/usr/bin/dirname "$0")"
-# . "$BIN_DIR/start.stage2.functions"
-# readonly CONFIG_FILE="$(var - CONFIG_FILE)"
-# readonly CONFIG_DIR="$(/usr/bin/dirname "$CONFIG_FILE")"
+# . /start/stage2.functions
 # ---------------------------------------------------------
+
+createPgagentExtension(){
+   local prio="010"
+   local dbname="postgres"
+   local sql_file="/init/initdb/$prio.$dbname.sql"
+   echo "CREATE EXTENSION IF NOT EXISTS pgagent;" > "$sql_file"
+}
 
 if [ "$VAR_CREATE_EXTENSION_PGAGENT" == "yes" ]
 then
-   prio="010"
-   dbname="postgres"
-   sql_file="/init/initdb/$prio.$dbname.sql"
-   echo "CREATE EXTENSION IF NOT EXISTS pgagent;" > "$sql_file"
+   createPgagentExtension
 fi
