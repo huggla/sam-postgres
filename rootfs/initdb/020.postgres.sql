25c7c5ce0a03fff4cf00ddd5731f9b9ea8b86d3e
Henrik Uggla <henrik.uggla@kristianstad.se>
2018-11-01 11:25:51 +0100

    Delete 020.postgres.sql

--- rootfs/initdb/020.postgres.sql
+++
@@ -1,4 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS postgis;
-CREATE EXTENSION IF NOT EXISTS postgis_topology;
-CREATE DATABASE template_postgis;
-UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template_postgis';
