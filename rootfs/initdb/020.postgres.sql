23dd1116e5d56884655c0b9291b8fbabafc87ad3
Henrik Uggla <henrik.uggla@kristianstad.se>
2018-11-05 15:34:05 +0100

    Update 020.postgres.sql

--- rootfs/initdb/020.postgres.sql
+++ rootfs/initdb/020.postgres.sql
@@ -1,4 +1,5 @@
 CREATE EXTENSION IF NOT EXISTS postgis;
 CREATE EXTENSION IF NOT EXISTS postgis_topology;
+CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
 CREATE DATABASE template_postgis;
 UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template_postgis';
