a9b78b941173f8407b8fde6c2c70a4c59d4c48ae
Henrik Uggla <henrik.uggla@kristianstad.se>
2019-02-28 10:13:35 +0100

    Create 020.postgres.sql

---
+++ postgis/rootfs/initdb/020.postgres.sql
@@ -0,0 +1,6 @@
+CREATE EXTENSION IF NOT EXISTS postgis;
+CREATE EXTENSION IF NOT EXISTS postgis_topology;
+CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
+CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
+CREATE DATABASE template_postgis;
+UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template_postgis';
